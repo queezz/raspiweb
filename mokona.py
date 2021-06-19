a2c8a99d68b8fbe2a1388f88f60021621966ef99
queezz <queezz@gmail.com>
2021-06-19 19:55:59 +0900

    added phrases

--- mokona.py
+++ mokona.py
@@ -4,6 +4,7 @@ for details, visit:  http://mattrichardson.com/Raspberry-Pi-Flask/inde...
 """
 from flask import Flask, render_template
 import datetime
+import pandas as pd
 import os
 
 app = Flask(__name__)
@@ -13,8 +14,14 @@ app = Flask(__name__)
 def hello():
     now = datetime.datetime.now()
     timeString = now.strftime("%Y-%m-%d %H:%M")
-    templateData = {"title": "HELLO!", "time": timeString}
+    templateData = {"title": "Mokona", "time": timeString, "phrase": choosephrase()}
     return render_template("index.html", **templateData)
+
+
+def choosephrase():
+    bpth = os.path.dirname(os.path.realpath(__file__))
+    lines = pd.read_csv(os.path.join(bpth, "phrases.txt"), names=["phrase"])
+    return lines.sample().iloc[0][0]
 
 
 if __name__ == "__main__":
